8aa5f66634d280757e40faa0d175ea3b30d5a4cb
Gosia Krupa <mkrupa@dnb.no>
2021-02-17 09:23:23 +0100

    (feature) Adding code for Palindrome in C#

---
+++ Palindrome.cs
@@ -0,0 +1,19 @@
+using System;
+public class Example
+{
+   
+    public static bool checkPalindrome(string inputString)
+        {
+          char[] c = inputString.ToCharArray();
+          Array.Reverse(c);
+          return new string(c).Equals(inputString);
+        }
+    public static void Main()
+        {
+             Console.WriteLine(checkPalindrome("aaa"));
+             Console.WriteLine(checkPalindrome("abc"));
+             Console.WriteLine(checkPalindrome("madam"));
+             Console.WriteLine(checkPalindrome("1234"));
+             
+        }
+}
